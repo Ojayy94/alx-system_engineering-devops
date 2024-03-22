72cbcb4e20cdd4a0d96b3f0dc038823ac14cd2a5
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-03-22 08:21:08 -0700

    install

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -5,7 +5,21 @@
 # Install flask
 # Version must be 2.1.0
 
+exec { 'install pip3':
+  command => '/usr/bin/apt-get install -y pip3'
+}
+
+exec { 'install flask':
+  command => '/usr/bin/pip3 install flask -v 2.1.0'
+}
+
+package { 'pip3':
+  ensure => 'installed',
+  before => Exec['install pip3']
+}
+
 package { 'flask':
   ensure   => 'installed',
-  provider => 'pip3'
+  before  => Exec['install flask'],
+  require => Package['pip3']
 }
